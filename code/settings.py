e80bfe3a2aa4438277849e1d6c603e64ea4b4127
Patthanan <patthananchualam@gmail.com>
2022-11-11 01:03:23 +0700

    zero

---
+++ code/settings.py
@@ -0,0 +1,13 @@
+# game setup
+verical_tile_number = 120
+TILESIZE = 16
+
+WIDTH    = 800
+HEIGTH   = 600
+center = ((WIDTH/2),(HEIGTH/2))
+FPS      = 60
+
+#weapon data
+weapon_data = {
+    'Gun'  : {'cooldown' : 100, 'damage' : 20}
+}
